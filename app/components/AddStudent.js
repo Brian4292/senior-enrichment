71ff2bd0b69826975e8d2a8899d1c833bd70414a
Bderiel <bderiel@Brians-Air.T-mobile.com>
2017-10-16 00:37:19 -0400

    need to finish post request

--- app/components/AddStudent.js
+++ app/components/AddStudent.js
@@ -5,29 +5,58 @@ export default class AddStudent extends Component {
 	constructor() {
 		super();
 		this.state = {
-      students: []
+            campuses: [],
+            studentName: '',
+            studentEmail: '',
+            studentAccount: '',
+            selectedCampus: ''
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  componentDidMount() {
+    axios.get('api/campus')
+        .then(response => {
+            return response.data;
+        })
+        .then(campusList => {
+            this.setState({campuses: campusList});
+        })
+        .catch(err => {
+            console.error('error');
+            console.error(err);
+        });
+}
+
+handleSubmit(event){
+    event.preventDefault();
+    console.log(1)
+
+}
 
   render() {
 		return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label> Name:
                     <div>
                         <input type="text" name="name" />
                      </div>
                 </label>
-                <label> Email
+                <label> Email:
                     <div>
                         <input type="text" name="name" />
                         </div>
-                </label> Githb Account
+                </label> Githb Account:
                 <label>
                     <div>
                         <input type="text" name="name" />
                      </div>
-                        <input type="text" name="name" />
-                 </label> Campus
+                       <select>
+                       {this.state.campuses && this.state.campuses.map(campus=>{
+                           return (<option value={campus.id} key={campus.name}>{campus.name}</option>)
+                       })}
+                       </select>
+                 </label> Campus:
                 <input type="submit" value="Submit" />
           </form>
 		);
